c190d5739a9484df9fbdc81627d88ffde8cfe817
Chaz Kondo <Chaz@2ppgn4x.local>
2018-07-07 15:19:59 -1000

    Pushing secret number. Confused by the module exports

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+    return secretNumberGenerator();
 };
+
+function secretNumberGenerator(){
+    const randomNum = Math.floor(Math.random() * 1000000);
+    return function() {
+        return randomNum;
+    }
+}
+
